662bec831f10f2c39081be78c8778daa1d962591
Marcus Godwin <marcusgodwin@hotmail.com>
2015-05-17 00:50:33 -0400

    create gui window using swing

--- main.scala
+++ main.scala
@@ -1,5 +1,19 @@
+import javax.swing.JFrame
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+
 object Main {
   def main(args: Array[String]) = {
     println("Hello Scala World!")
+
+    val frame : JFrame = new JFrame("Project Evolve")
+    frame.addWindowListener(new WindowAdapter() {
+      override def windowClosing(windowevent : WindowEvent) = {
+        frame.dispose()
+        System.exit(0)
+      }
+    })
+    frame.setSize(640,480)
+    frame.setVisible(true)
   }
 }
